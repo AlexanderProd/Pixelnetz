e7c669beb07accdd14ad169765ae5829d35369df
michel <michel.strelow97@gmail.com>
2018-10-19 19:27:55 +0200

    Alle Seiten vom Server hosten

--- pixel_websockets/server/src/appConfig.js
+++ pixel_websockets/server/src/appConfig.js
@@ -1,5 +1,11 @@
-module.exports = app => app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+module.exports = (app, express) => {
+  app.use('/', express.static(__dirname + '../../../dist_frontend/'));
+
+  app.use('/master', express.static(__dirname + '../../../dist_master/'));
+
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+};
--- pixel_websockets/server/src/index.js
+++ pixel_websockets/server/src/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const hostname = require('./util/hostname.js');
 
+const PORT = 3000;
 const app = express();
 const clients = new Map();
 
-require('./appConfig.js')(app);
+require('./appConfig.js')(app, express);
 require('./routes/start.js')(app, clients);
 require('./ws.js')(clients);
 
-app.listen(3000, () => {
-  console.log('listening on port 3000...');
+app.listen(PORT, async () => {
+  console.log(`view app at localhost:${PORT} or ${await hostname()}:${PORT}`);
 });
