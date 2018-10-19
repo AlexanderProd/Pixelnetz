774664e111605a8f32ea85307611713fd646cbc1
michel <michel.strelow97@gmail.com>
2018-10-19 19:26:17 +0200

    Master Seite hinzufügen

---
+++ pixel_websockets/master/src/index.js
@@ -0,0 +1,18 @@
+window.onload = () => {
+  const start = document.createElement('button');
+  start.innerHTML = 'start';
+  start.onclick = () => fetch('/start')
+    .then(r => r.json())
+    .then(r => console.log(r))
+    .catch(err => console.error(err));
+  
+  const stop = document.createElement('button');
+  stop.innerHTML = 'stop';
+  stop.onclick = () => fetch('/stop')
+    .then(r => r.json())
+    .then(r => console.log(r))
+    .catch(err => console.error(err));
+  
+  document.body.appendChild(start);
+  document.body.appendChild(stop);
+};
