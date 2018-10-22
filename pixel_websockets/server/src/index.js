94a28c46a22a4cfd59f6955d4b667ed3699d1ade
michel <michel.strelow97@gmail.com>
2018-10-22 16:57:33 +0200

    Lokale IP-Adresse Plattformunabhängig vom Hostsystem erhalten

--- pixel_websockets/server/src/index.js
+++ pixel_websockets/server/src/index.js
@@ -1,18 +1,20 @@
 const express = require('express');
-const hostname = require('./util/hostname.js');
+const localIP = require('my-local-ip');
 
-(async () => {
-  const PORT = 3000;
-  const localHostname = await hostname();
-  const app = express();
-  const clients = new Map();
-  
-  require('./appConfig.js')(app, express);
-  require('./routes/start.js')(app, clients);
-  require('./routes/wshost.js')(app, localHostname);
-  require('./ws.js')(clients);
-  
-  app.listen(PORT, async () => {
-    console.log(`view app at localhost:${PORT} or ${localHostname}:${PORT}`);
-  });
-})();
+const PORT = 3000;
+const localHostname = localIP();
+const app = express();
+const clients = new Map();
+
+require('./appConfig.js')(app, express);
+require('./routes/start.js')(app, clients);
+require('./routes/wshost.js')(app, localHostname);
+require('./ws.js')(clients);
+
+app.listen(PORT, async () => {
+  console.log(
+    '\n' +
+    `Client Seite auf http://${localHostname}:${PORT} aufrufen.\n` +
+    `Steuerung der Animation unter http://${localHostname}:${PORT}/master.\n`
+  );
+});
