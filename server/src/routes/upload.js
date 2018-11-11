0e5cd5eb268516cc33a5276aed49b76b4c746535
Alexander Hoerl <alexander.hoerl@alexander-productions.de>
2018-11-11 17:51:22 +0100

    commented out unnecessary code

--- server/src/routes/upload.js
+++ server/src/routes/upload.js
@@ -1,7 +1,8 @@
 const upload = (app) => app.post('/upload', function(req, res) {
+  /*
   if (Object.keys(req.files).length == 0) {
     return res.status(400).send('No files were uploaded.');
-  }
+  }; */
 
   let sampleFile = req.files.sampleFile;
   let uploadPath = __dirname + '/uploads/' + sampleFile.name;
@@ -9,7 +10,7 @@ const upload = (app) => app.post('/upload', function(req, res) {
   sampleFile.mv(uploadPath, function(err) {
     if (err){
       return res.status(500).send(err);
-    }
+    };
     res.send('File uploaded!');
   });
 });
