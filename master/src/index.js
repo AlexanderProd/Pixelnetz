71b99117d2e831d60046ce1275efc5848a18ed10
Alexander Hoerl <alexander.hoerl@alexander-productions.de>
2018-11-11 08:54:11 +0100

    preperation for file upload

--- master/src/index.js
+++ master/src/index.js
@@ -5,6 +5,8 @@ import { PersistGate } from 'redux-persist/integration/react';
 import store, { persistor } from './redux/config';
 import App from './App';
 import './index.sass';
+import { useState } from 'react';
+import axios from 'axios';
 
 import getBaseUrl from '../../shared/util/baseUrl';
 
@@ -76,5 +78,31 @@ stop.onclick = () => fetch(`${baseUrl}/stop`, { headers: { 'x-access-token': tok
   .then(r => console.log(r))
   .catch(err => console.error(err));
 
+const [count, setCount] = useState(null);
+
+const file = document.createElement('input');
+file.type = 'file';
+file.onchange = this.handleselectedFile();
+
+const upload = document.createElement('button');
+upload.innerHTML = 'Upload';
+upload.onclick = () => {
+  const data = new FormData();
+  data.append('file', count.selectedFile, count.selectedFile.name);
+  axios.post(`${baseUrl}/upload`, data)
+  .then(res => {
+    console.log(res.statusText)
+  });
+};
+
+handleselectedFile = event => {
+  setCount({
+    selectedFile: event.target.files[0],
+    loaded: 0,
+  })
+};
+
 document.body.appendChild(start);
 document.body.appendChild(stop);
+document.body.appendChild(upload);
+document.body.appendChild(file);
