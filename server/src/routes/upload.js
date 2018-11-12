7a3794e37bcae9554580636b80122bbf48079435
Alexander Hoerl <alexander.hoerl@alexander-productions.de>
2018-11-12 12:26:52 +0100

    linter

--- server/src/routes/upload.js
+++ server/src/routes/upload.js
@@ -1,11 +1,8 @@
 const upload = (app) => app.post('/upload', function(req, res) {
-  /*
-  if (Object.keys(req.files).length == 0) {
-    return res.status(400).send('No files were uploaded.');
-  }; */
+  console.log(req);
 
-  let sampleFile = req.files.sampleFile;
-  let uploadPath = __dirname + '/uploads/' + sampleFile.name;
+  const sampleFile = req.files.sampleFile;
+  const uploadPath = `${__dirname}/uploads/${sampleFile.name}`;
 
   sampleFile.mv(uploadPath, function(err) {
     if (err){
