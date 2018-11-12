ba3248d0eecf56fc8742660b529b0f6d2db1d9b3
michel <michel.strelow97@gmail.com>
2018-11-12 16:34:05 +0100

    #8 Add test framework for master

--- master/webpack.config.js
+++ master/webpack.config.js
@@ -26,16 +26,7 @@ module.exports = (env, argv) => {
     module: {
       rules: [{
         test: /\.js$/,
-        use: [{
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              '@babel/preset-env',
-              '@babel/preset-react',
-            ],
-            plugins: ['@babel/plugin-syntax-dynamic-import'],
-          },
-        }],
+        use: ['babel-loader'],
       }, {
         test: /\.sass$/,
         use: [
