776e22f0d0f753cb3bbbe1c721b06ae641df6c93
michel <michel.strelow97@gmail.com>
2018-11-12 15:19:33 +0100

    #24 Optimize webpack bundeling

--- master/webpack.config.js
+++ master/webpack.config.js
@@ -2,54 +2,77 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const webpack = require('webpack');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-  entry: [
-    '@babel/polyfill',
-    './src/index.js',
-  ],
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist',
-  },
-  module: {
-    rules: [{
-      test: /\.js$/,
-      use: [{
-        loader: 'babel-loader',
-        options: {
-          presets: [
-            '@babel/preset-env',
-            '@babel/preset-react',
-          ],
-          plugins: ['@babel/plugin-syntax-dynamic-import'],
-        },
+module.exports = (env, argv) => {
+  const isDev = argv.mode === 'development';
+  const isProd = argv.mode === 'production';
+
+  return {
+    mode: isProd ? 'production' : 'development',
+    entry: [
+      '@babel/polyfill',
+      './src/index.js',
+    ],
+    devtool: isDev ? 'source-map' : false,
+    devServer: {
+      contentBase: './dist',
+      port: 8082,
+    },
+    module: {
+      rules: [{
+        test: /\.js$/,
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+            ],
+            plugins: ['@babel/plugin-syntax-dynamic-import'],
+          },
+        }],
+      }, {
+        test: /\.sass$/,
+        use: [
+          isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          'resolve-url-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
+      }, {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        exclude: /node_modules/,
+        loader: 'file-loader',
       }],
-    }, {
-      test: /\.sass$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        'resolve-url-loader',
-        'postcss-loader',
-        'sass-loader',
-      ],
-    }, {
-      test: /\.(eot|svg|ttf|woff|woff2)$/,
-      exclude: /node_modules/,
-      loader: 'file-loader',
-    }],
-  },
-  plugins: [
-    new CleanWebpackPlugin(['dist']),
-    new HtmlWebpackPlugin({
-      template: 'public/index.html',
-    }),
-    new webpack.HotModuleReplacementPlugin(),
-  ],
-  output: {
-    filename: '[name].[hash].bundle.js',
-    chunkFilename: '[name].[hash].chunk.js',
-    path: path.resolve(__dirname, '../dist/static/master'),
-  },
+    },
+    plugins: [
+      new CleanWebpackPlugin(['dist']),
+      new HtmlWebpackPlugin({
+        template: 'public/index.html',
+      }),
+      new MiniCssExtractPlugin({
+        filename: isDev ? '[name].css' : '[name].[hash].css',
+        chunkFilename: isDev ? '[id].css' : '[id].[hash].css',
+      }),
+      ...(isDev ? [new webpack.HotModuleReplacementPlugin()] : []),
+      new BundleAnalyzerPlugin({
+        analyzerMode: isDev ? 'server' : 'static',
+        analyzerPort: 8083,
+        openAnalyzer: false,
+        reportFilename: '../../reports/master.html',
+      }),
+    ],
+    output: {
+      filename: isDev
+        ? '[name].bundle.js'
+        : '[name].[chunkhash].bundle.js',
+      chunkFilename: isDev
+        ? '[name].chunk.js'
+        : '[name].[chunkhash].chunk.js',
+      path: path.resolve(__dirname, '../dist/static/master'),
+    },
+  };
 };
