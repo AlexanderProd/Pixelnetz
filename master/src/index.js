bd3aecada4933f13f11c006a743488d1691c7d03
michel <michel.strelow97@gmail.com>
2018-11-13 14:41:52 +0100

    #5 Add upload route to server

--- master/src/index.js
+++ master/src/index.js
@@ -5,7 +5,6 @@ import { PersistGate } from 'redux-persist/integration/react';
 import store, { persistor } from './redux/config';
 import App from './App';
 import './index.sass';
-import { useState } from 'react';
 
 import getBaseUrl from '../../shared/util/baseUrl';
 
@@ -77,23 +76,36 @@ stop.onclick = () => fetch(`${baseUrl}/stop`, { headers: { 'x-access-token': tok
   .then(r => console.log(r))
   .catch(err => console.error(err));
 
-const uploadForm = document.createElement('form');
-uploadForm.id = 'uploadForm';
-uploadForm.ref = 'uploadForm';
-uploadForm.action = `${baseUrl}/upload`;
-uploadForm.method = 'POST';
-uploadForm.encType = 'multipart/form-data';
-
 const file = document.createElement('input');
 file.type = 'file';
 file.name = 'sampleFile';
+
+let selectedFile = null;
+
+file.oninput = (e) => {
+  const { files } = e.target;
+  selectedFile = files[0];
+};
 
 const upload = document.createElement('input');
 upload.type = 'submit';
 upload.value = 'upload';
 
+upload.onclick = () => {
+  const formData = new FormData();
+  formData.append('file', selectedFile, 'sampleFile.png');
+
+  fetch(`${baseUrl}/upload`, {
+    method: 'POST',
+    'Content-Type': 'image/png',
+    body: formData,
+  })
+    .then(r => r.text())
+    .then(r => console.log(r))
+    .catch(e => console.log(e));
+};
+
 document.body.appendChild(start);
 document.body.appendChild(stop);
-uploadForm.appendChild(file);
-uploadForm.appendChild(upload);
-document.body.appendChild(uploadForm);
+document.body.appendChild(file);
+document.body.appendChild(upload);
--- server/src/routes/upload.js
+++ server/src/routes/upload.js
@@ -1,15 +1,6 @@
-const upload = (app) => app.post('/upload', function(req, res) {
-  console.log(req);
-
-  const sampleFile = req.files.sampleFile;
-  const uploadPath = `${__dirname}/uploads/${sampleFile.name}`;
-
-  sampleFile.mv(uploadPath, function(err) {
-    if (err){
-      return res.status(500).send(err);
-    };
-    res.send('File uploaded!');
-  });
+const upload = (app) => app.post('/upload', (req, res) => {
+  const { file } = req.files;
+  res.sendStatus(200);
 });
 
 export default upload;
