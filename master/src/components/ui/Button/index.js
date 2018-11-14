c2075b2d0a57947b477860978a2976ec8aa57009
michel <michel.strelow97@gmail.com>
2018-11-14 16:33:07 +0100

    #25 Add reusable Button component

---
+++ master/src/components/ui/Button/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Icon from '../Icon';
+import './Button.sass';
+
+const propTypes = {
+  type: PropTypes.oneOf([
+    'button',
+    'submit',
+    'reset',
+  ]),
+  children: PropTypes.node,
+  icon: PropTypes.string,
+};
+
+const defaultProps = {
+  type: 'button',
+  children: null,
+  icon: null,
+};
+
+const Button = ({ children, type, icon, ...props }) => (
+  // eslint-disable-next-line react/button-has-type
+  <button className="Button" type={type} {...props}>
+    {icon ? <Icon name={icon} /> : children}
+  </button>
+);
+
+Button.propTypes = propTypes;
+Button.defaultProps = defaultProps;
+
+export default Button;
