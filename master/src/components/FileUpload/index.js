01d900c9b30ea5b71f400efa540295db5f3628b7
michel <michel.strelow97@gmail.com>
2018-11-15 21:17:00 +0100

    #25 Add dropzone for file upload

--- master/src/components/FileUpload/index.js
+++ master/src/components/FileUpload/index.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Form } from '../ui';
+import Dropzone from 'react-dropzone';
+import { Form, Icon, Button } from '../ui';
+import './FileUpload.sass';
 
 const propTypes = {
 
@@ -10,11 +12,77 @@ const defaultProps = {
 
 };
 
-const FileUpload = props => (
-  <Form className="FileUpload">
-    asd
-  </Form>
-);
+const FileUpload = (props) => {
+  const [file, setFile] = useState(null);
+  const [rejectedFile, setRejectedFile] = useState(null);
+  const [multipleDropped, setMultipleDropped] = useState(false);
+
+  const handleDrop = ([accepted], rejected) => {
+    if (accepted) {
+      setFile(accepted);
+      setRejectedFile(null);
+      setMultipleDropped(false);
+    }
+    if (rejected.length === 1) {
+      setFile(null);
+      setRejectedFile(rejected[0]);
+      setMultipleDropped(false);
+    }
+    if (rejected.length > 1) {
+      setFile(null);
+      setMultipleDropped(true);
+    }
+  };
+
+  const handleCancel = () => {
+    setFile(null);
+    setRejectedFile(null);
+    setMultipleDropped(false);
+  };
+
+  return (
+    <Form className="FileUpload">
+      <Dropzone
+        className="upload-dropzone"
+        activeClassName="upload-dropzone-active"
+        onDrop={handleDrop}
+        accept=".png,.jpg,.jpeg"
+        multiple={false}
+      >
+        <Icon name="file-upload" />
+        <div className="d-and-d">
+          DRAG
+          {' & '}
+          DROP
+        </div>
+        <div className="browse-for-file">or browse for file</div>
+      </Dropzone>
+      <Button type="submit">Upload</Button>
+      <Button type="reset" onClick={handleCancel}>Cancel</Button>
+      <div className="upload-info">
+        {file && (
+          <span>
+            File
+            {` "${file.name}" `}
+            is ready for upload.
+          </span>
+        )}
+        {rejectedFile && (
+          <span>
+            File
+            {` "${rejectedFile.name}" `}
+            cannot be uploaded.
+          </span>
+        )}
+        {multipleDropped && (
+          <span>
+            Please only drop one file.
+          </span>
+        )}
+      </div>
+    </Form>
+  );
+};
 
 FileUpload.propTypes = propTypes;
 FileUpload.defaultProps = defaultProps;
