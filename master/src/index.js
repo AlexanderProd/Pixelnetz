e0aed09cd716135bf14c3cc3870a08e07d389f09
michel <michel.strelow97@gmail.com>
2018-11-16 17:45:29 +0100

    #25 Remove last non react component

--- master/src/index.js
+++ master/src/index.js
@@ -6,8 +6,6 @@ import store, { persistor } from './redux/config';
 import App from './App';
 import './index.sass';
 
-import getBaseUrl from '../../shared/util/baseUrl';
-
 ReactDOM.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
@@ -16,38 +14,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root'),
 );
-
-
-const baseUrl = getBaseUrl();
-
-const file = document.createElement('input');
-file.type = 'file';
-file.name = 'sampleFile';
-
-let selectedFile = null;
-
-file.oninput = (e) => {
-  const { files } = e.target;
-  selectedFile = files[0];
-};
-
-const upload = document.createElement('input');
-upload.type = 'submit';
-upload.value = 'upload';
-
-upload.onclick = () => {
-  const formData = new FormData();
-  formData.append('file', selectedFile, 'sampleFile.png');
-
-  fetch(`${baseUrl}/upload`, {
-    method: 'POST',
-    'Content-Type': 'image/png',
-    body: formData,
-  })
-    .then(r => r.text())
-    .then(r => console.log(r))
-    .catch(e => console.log(e));
-};
-
-document.body.appendChild(file);
-document.body.appendChild(upload);
