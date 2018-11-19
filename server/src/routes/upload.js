1839c589184f2cad38b68a20919de716cdb257ae
michel <michel.strelow97@gmail.com>
2018-11-19 20:39:49 +0100

    #21 Save uploaded sequences

--- server/src/routes/upload.js
+++ server/src/routes/upload.js
@@ -1,13 +1,29 @@
+import fs from 'fs';
+import { promisify } from 'util';
 import rasterize from '../sequences/rasterize';
+import toMatrix from '../sequences/toMatrix';
+
+const writeFile = promisify(fs.writeFile);
 
 const upload = () => (req, res) => {
   const { file } = req.files;
-  res.sendStatus(200);
+
   rasterize(file.data)
-    .then((pixelMatrix) => {
-      pixelMatrix.forEach(row => console.log(row.join()));
+    .then(pixelMatrix => pixelMatrix.map(
+      row => toMatrix(row, 1),
+    ))
+    .then((sequenceMatrix) => writeFile(
+      `${__dirname}/../sequences/db/${file.name}.json`,
+      JSON.stringify(sequenceMatrix),
+    ))
+    .then(() => {
+      console.log('file written successfully');
+      res.sendStatus(200);
     })
-    .catch(err => console.log(err));
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
 };
 
 export default upload;
