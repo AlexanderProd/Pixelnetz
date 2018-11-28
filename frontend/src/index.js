f1812c5e81f116997bc948a2c91713ca88dd050e
michel <michel.strelow97@gmail.com>
2018-11-28 12:01:27 +0100

    #12 Refactor browser configuration

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,11 @@ import '../polyfills';
 import baseUrl from '../../shared/util/baseUrl';
 import createSender from '../../shared/util/createSender';
 import createActionRunner from './actions';
-import enableNoSleep from '../src/util/browserModules';
-// import openFullscreen from '../src/util/browserModules';
+import configureBrowser from '../src/util/browserModules';
 
 const main = async () => {
+  configureBrowser();
+
   const { hostname } = await (fetch(`${baseUrl()}/wshost`).then(r => r.json()));
   const socket = new WebSocket(`ws://${hostname}:3001`);
   const send = createSender(socket);
@@ -15,11 +16,6 @@ const main = async () => {
     const message = JSON.parse(data);
     runAction(message);
   };
-
-  console.log('indexFrontend');
-  const elem = document.getElementById('toggle');
-  elem.addEventListener('click', enableNoSleep, false);
-  // openFullscreen(document.getElementById('toggle'));
 };
 
 main();
--- frontend/src/util/browserModules.js
+++ frontend/src/util/browserModules.js
@@ -1,16 +1,42 @@
 import NoSleep from 'nosleep.js';
+import fullscreen from 'fullscreen';
 
+const fs = fullscreen(document.documentElement);
 const noSleep = new NoSleep();
 
-const enableNoSleep = () => {
-  console.log('enableNoSleep');
-  noSleep.enable();
-  document.removeEventListener('click', enableNoSleep, false);
-  document.getElementById('toggle').style.display = ('none');
+const configureBrowser = () => {
+  const enableButton = document.createElement('input');
+  enableButton.type = 'button';
+  enableButton.value = 'Please put your brightness to max and click here!';
+
+  const disableButton = document.createElement('input');
+  disableButton.type = 'button';
+  disableButton.value = 'exit';
+  disableButton.style.display = 'none';
+
+  enableButton.onclick = () => {
+    fs.request();
+    noSleep.enable();
+  };
+
+  disableButton.onclick = () => {
+    fs.release();
+    noSleep.disable();
+  };
+
+  fs.on('attain', () => {
+    enableButton.style.display = 'none';
+    disableButton.style.display = 'inline';
+  });
+
+  fs.on('release', () => {
+    disableButton.style.display = 'none';
+    enableButton.style.display = 'inline';
+  });
+
+  document.body.appendChild(enableButton);
+  document.body.appendChild(disableButton);
 };
 
-// const elem = document.getElementById('toggle');
-
-
-export default enableNoSleep; // openFullscreen;
+export default configureBrowser;
 
