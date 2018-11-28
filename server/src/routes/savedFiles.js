3bc207df4715f988052c92c3517582651e99aaba
larissa2810 <lala.schmauss@web.de>
2018-11-28 13:35:55 +0100

    #40 add fileList route

--- server/src/routes/savedFiles.js
+++ server/src/routes/savedFiles.js
@@ -3,7 +3,6 @@ import { readdirSync } from 'fs';
 const savedFiles = () => (req, res) => {
   const fileList = readdirSync(`${__dirname}/../../db/`)
     .map(fileName => fileName.split('.json')[0]);
-  console.log(fileList);
   res.status(200).json(fileList);
 };
 
