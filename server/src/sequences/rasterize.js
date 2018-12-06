ae2a3f39906158774fae8c349c29859d2d62d8f1
michel <michel.strelow97@gmail.com>
2018-12-06 10:00:39 +0100

    #5 Fix missing rows

--- server/src/sequences/rasterize.js
+++ server/src/sequences/rasterize.js
@@ -1,22 +1,20 @@
 import { promisify } from 'util';
-import chroma from 'chroma-js';
 import getPixelsCB from 'get-pixels';
 import sharp from 'sharp';
-import toMatrix from './toMatrix';
 
 const getPixels = promisify(getPixelsCB);
 
 const toHex = num => num.toString(16).padStart(2, '0');
 
 const rasterize = (buffer, mimetype) => sharp(buffer)
-  .resize(10)
+  .resize(100)
   .toBuffer()
   .then(b => getPixels(b, mimetype))
   .then(({ data, shape }) => {
-    const [width, height, channels] = shape;
+    const [width,, channels] = shape;
 
     const matrix = [];
-    for (let pos = 0; pos < (data.length / channels); pos += channels) {
+    for (let pos = 0; pos < data.length; pos += channels) {
       const r = data[pos];
       const g = data[pos + 1];
       const b = data[pos + 2];
@@ -25,6 +23,7 @@ const rasterize = (buffer, mimetype) => sharp(buffer)
       const i = pos / channels;
       const isRowStart = i % width === 0;
       const rowIndex = Math.floor(i / width);
+
       if (isRowStart) {
         matrix.push([[[col, 1]]]);
       } else {
@@ -32,14 +31,7 @@ const rasterize = (buffer, mimetype) => sharp(buffer)
       }
     }
 
-    console.log(matrix);
-
-    const colorStrip = toMatrix(data, channels)
-      .map(([r, g, b]) => chroma(r, g, b).hex());
-    return toMatrix(colorStrip, width);
-  })
-  .then(pixelMatrix => pixelMatrix.map(
-    row => row.map(col => [[col, 1]]),
-  ));
+    return matrix;
+  });
 
 export default rasterize;
