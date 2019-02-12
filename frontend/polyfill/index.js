28e40971eae729c8d8d023052d501311f530aefa
michel <michel.strelow97@gmail.com>
2019-02-12 10:53:01 +0100

    fullscreen entfernen

--- frontend/polyfill/index.js
+++ frontend/polyfill/index.js
@@ -1,14 +1,10 @@
 import P from 'es6-promise';
-import { fetch as f } from 'whatwg-fetch';
 import 'raf/polyfill';
 import 'iterators-polyfill';
 import './ArrayFrom';
 import './ObjectAssign';
+import './Classlist';
 
 if (Promise in window) {
   window.Promise = P;
-}
-
-if (fetch in window) {
-  window.fetch = f;
 }
--- frontend/src/dialogs/index.js
+++ frontend/src/dialogs/index.js
@@ -3,15 +3,11 @@ import './dialog.sass';
 
 const runConfigDialogs = () => {
   const enableNoSleepImport = import('./enableNoSleep');
-  const fullscreenConfigImport = import('./fullscreen');
 
   screenBrightness(async () => {
     const enableNoSleep = (await enableNoSleepImport).default;
 
-    enableNoSleep(async () => {
-      const fullscreenConfig = (await fullscreenConfigImport).default;
-      fullscreenConfig();
-    });
+    enableNoSleep();
   });
 };
 
