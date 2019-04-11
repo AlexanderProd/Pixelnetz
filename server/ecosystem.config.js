17a5224ec22db94f4f476da31858eda7c1acc123
michel <michel.strelow97@gmail.com>
2019-04-11 12:48:37 +0200

    #56 Change dist path

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,18 +1,21 @@
 /* eslint-disable */
 module.exports = {
-  apps : [{
-    name: 'pixelnetz-server',
-    script: 'dist/index.js',
+  apps: [
+    {
+      name: 'pixelnetz-server',
+      script: 'dist/server/src/index.js',
 
-    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-    instances: 1,
-    autorestart: true,
-    watch: false,
-    env: {
-      NODE_ENV: 'production',
-      ADMIN_PW: 'admin',
-      JWT_SECRET: '2psPxwr0n3z4R95vIocgRabt1AJZ9jH6bJHrZUoR6V9Mq0poilWbFqkddbYq7Upf',
-      SALT_ROUNDS: '10'
+      // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      env: {
+        NODE_ENV: 'production',
+        ADMIN_PW: 'admin',
+        JWT_SECRET:
+          '2psPxwr0n3z4R95vIocgRabt1AJZ9jH6bJHrZUoR6V9Mq0poilWbFqkddbYq7Upf',
+        SALT_ROUNDS: '10',
+      },
     },
-  }],
+  ],
 };
