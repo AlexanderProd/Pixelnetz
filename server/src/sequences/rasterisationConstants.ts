f9958309d26ff3ee0286b6dd77d72715555abb0f
michel <michel.strelow97@gmail.com>
2019-05-02 14:14:26 +0200

    Fix rasterization constant type

--- server/src/sequences/rasterisationConstants.ts
+++ server/src/sequences/rasterisationConstants.ts
@@ -1,7 +1,9 @@
 // eslint-disable-next-line import/no-mutable-exports
-export let RESOLUTION = process.env.RASTERIZATION_RESOLUTION || 100;
+export let RESOLUTION: number =
+  Number(process.env.RASTERIZATION_RESOLUTION) || 100;
 // eslint-disable-next-line import/no-mutable-exports
-export let MAX_FRAMES = process.env.RASTERIZATION_MAX_FRAMES || 50;
+export let MAX_FRAMES: number =
+  Number(process.env.RASTERIZATION_MAX_FRAMES) || 50;
 
 export function setConstant(
   c: 'RESOLUTION' | 'MAX_FRAMES',
