0c7474f158d6bbb8259f10df1d3c7fb995180ee7
michel <michel.strelow97@gmail.com>
2019-05-17 13:46:57 +0200

    Server: Add testscript for rusterizer

---
+++ server/src/sequences/__testdata__/f.ts
@@ -0,0 +1,25 @@
+import fetch from 'node-fetch';
+import FormData from 'form-data';
+import fs from 'fs';
+import png from './large_jpeg';
+
+// const fd = new FormData();
+// fd.append('data', png);
+
+const gif = fs.readFileSync(
+  `${__dirname}/../../../test_images/sine.gif`,
+);
+const start = Date.now();
+fetch(
+  'http://localhost:8000/rasterize?format=gif&bit_depth=8&max_width=100',
+  {
+    method: 'POST',
+    body: gif,
+  },
+)
+  .then(r => r.text())
+  .then(r => {
+    // fs.writeFileSync(`${__dirname}/y.matrix.0.json`, r, 'utf-8');
+    console.log(r.length);
+    console.log(`\n\n>> ${Date.now() - start}`);
+  });
