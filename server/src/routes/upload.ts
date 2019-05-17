ad8a72efbbe8c97f26d2d5965133d2270a56dc49
michel <michel.strelow97@gmail.com>
2019-05-17 13:55:44 +0200

    Server: Measure rasterization time

---
+++ server/src/routes/upload.ts
@@ -0,0 +1,52 @@
+import { Request, Response } from 'express';
+import { isSafeFileName } from '../util/userInput';
+import sendAllSequences from '../util/sendAllSequences';
+import Sequence from '../sequences/Sequence';
+import Mimetypes from '../sequences/mimetypes';
+import MasterPool from '../ws/MasterPool';
+
+const allowedTypes = [Mimetypes.PNG, Mimetypes.JPEG, Mimetypes.GIF];
+
+const upload = (masterPool: MasterPool) => (
+  req: Request,
+  res: Response,
+) => {
+  if (!(req.files && req.files.file)) {
+    res.status(400).json({ error: 'No file found' });
+    return;
+  }
+
+  const { file } = req.files;
+
+  if (Array.isArray(file)) {
+    res
+      .status(400)
+      .json({ error: `Expected one file, found ${file.length}` });
+    return;
+  }
+
+  if (!isSafeFileName(file.name)) {
+    res.status(400).json({ error: 'Invalid sequence name' });
+    return;
+  }
+
+  if (!allowedTypes.includes(file.mimetype as Mimetypes)) {
+    res.status(400).json({ error: 'Invalid file format' });
+    return;
+  }
+
+  const start = Date.now();
+  Sequence.fromFile({ file, repeat: false })
+    .then(() => {
+      const seconds = (Date.now() - start) / 1000;
+      console.log(`file written successfully in ${seconds} sec`);
+      res.sendStatus(200);
+      sendAllSequences(masterPool);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
+};
+
+export default upload;
--- server/src/util/userInput.ts
+++ server/src/util/userInput.ts
@@ -1,14 +1,16 @@
-const safeChars = 'abcdefghijklmnopqrstuvwxyz' +
+const safeChars =
+  'abcdefghijklmnopqrstuvwxyz' +
   'ABCDEFGHIJKLMNOPQRSTUVWXYZ' +
   '1234567890' +
   '_';
 
 const MAX_LENGTH = 50;
 
-export const isSafeFileName = name => {
+// eslint-disable-next-line import/prefer-default-export
+export function isSafeFileName(name: string): boolean {
   if (name.length > MAX_LENGTH) return false;
   for (const char of name) {
     if (!safeChars.includes(char)) return false;
   }
   return true;
-};
+}
