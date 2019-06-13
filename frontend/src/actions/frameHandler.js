bca2ed7d889ebd2b0ef8e64f9e80ed1aa96337d4
michel <michel.strelow97@gmail.com>
2019-06-13 19:19:16 +0200

    #14 Frontend: Check for sound in sequence

--- frontend/src/actions/frameHandler.js
+++ frontend/src/actions/frameHandler.js
@@ -6,13 +6,21 @@ function createFrameHandler(sequence) {
     sequence.bitDepth,
   );
 
+  const withSound = !!sequence.soundFile;
+
+  if (withSound) {
+    player.selectSound(sequence.soundFile);
+  }
+
   const setColor = encodedColor => {
     const [r, g, b] = decodeColor(encodedColor);
     document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-    if (r + g + b > (255 * 3) / 2) {
-      player.setVolume(1);
-    } else {
-      player.setVolume(0);
+    if (withSound) {
+      if (r + g + b > (255 * 3) / 2) {
+        player.setVolume(1);
+      } else {
+        player.setVolume(0);
+      }
     }
   };
 
