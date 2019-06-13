c7f150b2f5c2484112b7e53e13da8302b84338ef
michel <michel.strelow97@gmail.com>
2019-06-13 18:08:07 +0200

    #14 Frontend: Improve chunk loading

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,11 +6,13 @@ import BASE_URL from './util/baseUrl';
 import './index.sass';
 
 const main = async () => {
+  const actionsImport = import(
+    /* webpackChunkName: 'actions' */ './actions'
+  );
+
   runConfigDialogs();
 
-  const createActionRunner = (await import(
-    /* webpackChunkName: 'actions' */ './actions'
-  )).default;
+  const createActionRunner = (await actionsImport).default;
   const socket = new WebSocket(`ws://${BASE_URL}/`);
   const send = createSender(socket);
   const runAction = createActionRunner(send);
